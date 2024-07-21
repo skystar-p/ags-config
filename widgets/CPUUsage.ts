96c4cfa74ae2bf3a61b2bc6b3c9a44714747df33
Jaehyeon Park <skystar@skystar.dev>
2024-07-21 15:46:01 +0900

    add transparentize effect to bar buttons

--- widgets/CPUUsage.ts
+++ widgets/CPUUsage.ts
@@ -1,19 +1,20 @@
 import cpu from "services/cpu";
 import icons from "utils/icons";
+import PanelButton from "./bar/PanelButton";
 
 export default () => {
-  return Widget.Box({
+  return PanelButton({
+    class_name: "cpu",
     css: "padding: 4px",
-    children: [
+    child: Widget.Box([
       Widget.Icon({
         css: "margin-right: 4px",
         icon: icons.system.cpu,
       }),
       Widget.Label({
         css: "font-size: 0.85em",
-        class_name: "cpu",
         label: cpu.bind("usage").as(v => `${v.toFixed(1)}%`),
       }),
-    ],
+    ]),
   });
 };
--- widgets/MemoryUsage.ts
+++ widgets/MemoryUsage.ts
@@ -1,19 +1,20 @@
 import memory from "services/memory";
 import icons from "utils/icons";
+import PanelButton from "./bar/PanelButton";
 
 export default () => {
-  return Widget.Box({
+  return PanelButton({
     css: "padding: 4px",
-    children: [
+    class_name: "memory",
+    child: Widget.Box([
       Widget.Icon({
         css: "margin-right: 4px",
         icon: icons.system.memory,
       }),
       Widget.Label({
         css: "font-size: 0.85em",
-        class_name: "memory",
         label: memory.bind("usage_kb").as(v => `${(v / 1024 / 1024).toFixed(1)} GB`),
       }),
-    ],
+    ]),
   });
 };
