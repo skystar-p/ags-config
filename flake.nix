2630d115954c4ac87073f9d700732ad8b975d4e9
Jaehyeon Park <skystar@skystar.dev>
2024-10-25 21:35:15 +0900

    update swww to git

--- flake.nix
+++ flake.nix
@@ -5,15 +5,17 @@
     ags.url = "github:skystar-p/ags/skystar";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    swww.url = "github:LGFae/swww";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, swww, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      swww-git = swww.packages.${system}.swww;
     in
     {
       packages = {
-        default = (pkgs.callPackage (import ./default.nix) { inherit inputs; });
+        default = (pkgs.callPackage (import ./default.nix) { inherit inputs; swww = swww-git; });
       };
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
